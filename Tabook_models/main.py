74da4c5cb700ed562945a358ed9853bdb2a49404
Qian Xiong <qx2dk@virginia.edu>
2016-02-23 18:59:40 -0500

    change HttpResponse to JsonResponse; refactor

--- Tabook_models/main.py
+++ Tabook_models/main.py
@@ -1,8 +1,5 @@
-import json
-
 from Tabook_models.forms import UserCreationForm
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 from .models import User
 
 
@@ -11,47 +8,46 @@ def reset_user_info(request):
     if request.method == "POST":
         if request.POST['id']:
             try:
-                id = request.POST['id']
-                u = User.objects.get(pk=id)
+                uid = request.POST['id']
+                user = User.objects.get(pk=uid)
+            except User.DoesNotExist:
+                content['result'] = "User not found"
+            else:
+                content['changed'] = []
                 if 'username' in request.POST:
                     username = request.POST['username']
-                    u.username = username
-                    u.save()
-                    content['success'] = True
+                    user.username = username
+                    content['changed'] += 'username'
                 if 'email' in request.POST:
                     email = request.POST['email']
-                    u.email = email
-                    u.save()
-                    content['success'] = True
+                    user.email = email
+                    content['changed'] += 'email'
                 if 'phone' in request.POST:
                     phone = request.POST['phone']
-                    u.phone = phone
-                    u.save()
-                    content['success'] = True
-            except:
-                content['result'] = "User not found"
+                    user.phone = phone
+                    content['changed'] = 'phone'
+                user.save()
+                content['success'] = True
         else:
             content['result'] = "User id not provided"
     else:
         content['result'] = "Invalid request method"
-
-    return HttpResponse(content=json.dumps(content), content_type="application/json")
-
+    return JsonResponse(content)
 
 
 def get_user(request, id):
-    user = None
-    content = {"success":False}
+    content = {"success": False}
     if request.method == 'GET':
         try:
             user = User.objects.get(pk=id)
-            content["success"] = True
-            for key,value in {'id': user.id, 'username': user.username, 'email': user.email, 'phone': user.phone}.items():
-                content[key] = value
-        except:
+        except User.DoesNotExist:
             content["result"] = "user not found"
-
-    return HttpResponse(content=json.dumps(content), content_type="application/json")
+        else:
+            content["success"] = True
+            for key, value in {'id': user.id, 'username': user.username, 'email': user.email,
+                               'phone': user.phone}.items():
+                content[key] = value
+    return JsonResponse(content)
 
 
 def create_user(request):
@@ -68,4 +64,4 @@ def create_user(request):
     else:
         content['result'] = "Invalid request method"
         pass  # do no respond
-    return HttpResponse(content=json.dumps(content), content_type="application/json")
+    return JsonResponse(content)
