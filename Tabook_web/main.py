15f092e5615d3902bda6fc2bce26b8d21e364c58
Qian Xiong <qx2dk@virginia.edu>
2016-02-28 00:23:28 -0500

    Show recommended restaurant names on home page

--- Tabook_web/main.py
+++ Tabook_web/main.py
@@ -1,10 +1,16 @@
-import json
-
+import requests
+from django.conf import settings
 from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, 'index.html')
+    context = {}
+    # get recommended url
+    url = settings.EXP_LAYER_URL + "restaurants/recommendation/"
+    data = requests.get(url)
+    # print(data.json())
+    context['recommended_restaurants'] = data.json()['result']
+    return render(request, 'index.html', context=context)
 
 
 def restaurant_details(request):
