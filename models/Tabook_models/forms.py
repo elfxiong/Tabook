48548061ac0c86b6f19d46f3bcd4ffb684d91f17
Dellvan7 <jacob.freck@gmail.com>
2016-03-16 17:38:34 -0400

    created TODO for forms evaluations

--- models/Tabook_models/forms.py
+++ models/Tabook_models/forms.py
@@ -1,17 +1,23 @@
-from Tabook_models.models import Customer, Restaurant, Review
+from .models import Customer, Restaurant, Review
 from django import forms
 
+#TODO: override to check for duplicate usernames
 
 class CustomerCreationForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
 
+    def validate(self):
+        pass
 
 class RestaurantCreationForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = '__all__'
+
+    def validate(self):
+        pass
 
 class ReviewCreationForm(forms.ModelForm):
     class Meta:
