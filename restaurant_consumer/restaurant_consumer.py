69f0c9f30be0929723c51e2b39c41f3841ec9267
Eliza1107 <yn9sa@virginia.edu>
2016-04-03 15:41:20 -0400

    fixed restaurant_consumer.py

--- restaurant_consumer/restaurant_consumer.py
+++ restaurant_consumer/restaurant_consumer.py
@@ -6,23 +6,24 @@ consumer = None
 es = None
 
 while not consumer or not es:
-	try:
-		consumer  = KafkaConsumer('new-restaurant-topic', group_id='restaurant-indexer', bootstrap_servers=['kafka:9092'])
-		es = Elasticsearch(['es'])
-	except:
-		print('fail to create restaurant topic')
-		pass
+    try:
+        consumer  = KafkaConsumer('new-restaurant-topic', group_id='restaurant-indexer', bootstrap_servers=['kafka:9092'])
+        es = Elasticsearch(['es'])
+    except:
+        print('fail to create restaurant topic')
+        pass
 print('test')
 for message in consumer:
-	print("restuanrt for loop")
+    print("restuanrt for loop")
     new_listing = json.loads((message.value).decode('utf-8'))
+    print("")
 
     # resaurant_id = new_listing['resaurant_id']
     # restaurant_name = new_listing['restaurant_name']
     # address = new_listing['address']
     # price_range = new_listing['price_range']
     # category = new_listing['category']
-	#
+    #
     # monday_open_time = new_listing['monday_open_time']
     # monday_close_time = new_listing['monday_close_time']
     # tuesday_open_time = new_listing['tuesday_open_time']
@@ -37,7 +38,7 @@ for message in consumer:
     # saturday_close_time = new_listing['saturday_close_time']
     # sunday_open_time = new_listing['sunday_open_time']
     # sunday_close_time = new_listing['sunday_close_time']
-	#
+    #
     # some_new_listing = {
     #     'resaurant_id':resaurant_id,
     #     'restaurant_name':restaurant_name,
