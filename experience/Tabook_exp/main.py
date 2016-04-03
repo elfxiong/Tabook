106a85886bbd359e6f087580ac37ac523db362c9
Qian Xiong <qx2dk@virginia.edu>
2016-04-03 18:00:23 -0400

    cleanup

--- experience/Tabook_exp/main.py
+++ experience/Tabook_exp/main.py
@@ -14,6 +14,7 @@ def get_restaurant(request):
     return JsonResponse(r.json())
 
 
+# TODO This method needs cleanup. Or we just remove it because we don't need it.
 def create_restaurant(request):
     content = {"success": False}
     if request.method != "POST":
@@ -112,7 +113,7 @@ def search_restaurant(request):
     content = {'success': False}
     query = request.GET['query']
     es = Elasticsearch(['es'])
-    es.indices.refresh(index='restaurant_index')
+    # es.indices.refresh(index='restaurant_index')
     result = es.search(index='restaurant_index', body={'query': {'query_string': {'query': query}}, 'size': 10})
     content['success'] = True
     content['result'] = [hit['_source'] for hit in result['hits']['hits']]
--- reservation_consumer/reservation_consumer.py
+++ reservation_consumer/reservation_consumer.py
@@ -9,20 +9,13 @@ while not consumer or not es:
 	try:
 		consumer  = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 		es = Elasticsearch(['es'])
+	    print('successfully created reservation topic')
 	except:
-		pass
+		print('fail to create restaurant topic')
 print('test')
 for message in consumer:
 	print("in reservation for loop")
 	new_listing = json.loads((message.value).decode('utf-8'))
 	print(new_listing)
-	# customer_id = new_listing['customer_id']
-	# table_id = new_listing['table']
-	# reservation_id = new_listing['reservation_id']
-	# start_time = new_listing['start_time']
-	# end_time = new_listing['end_time']
-	# created_time = new_listing['created_time']
-	# restaurant_name = new_listing['restaurant_name']
-	# some_new_listing = { 'customer_id':customer_id, 'table_id':table_id,'start_time':start_time, 'end_time': end_time, 'created_time':created_time, 'restaurant_name':restaurant_name}
 	es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
 	es.indices.refresh(index='listing_index')
